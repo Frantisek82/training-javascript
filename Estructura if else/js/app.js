3ddb25eff5ec67a03e12bedd789c12033a40c9e7
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-03-29 23:11:45 +0100

    organized exercises

---
+++ Estructura if else/js/app.js
@@ -0,0 +1,22 @@
+//Estructura if / else
+
+let totalBet = prompt("Please enter your bet");
+// console.log(totalBet);
+if (totalBet == 5000) {
+  alert("All-in!");
+} else if (totalBet < 5000) {
+  alert("Passing this round");
+} else {
+  alert("Wrong bet, try again!");
+  while (totalBet > 5000) {
+    totalBet = prompt("Please enter your bet");
+    if (totalBet > 5000) {
+      alert("Wrong bet, try again!");
+      totalBet = prompt("Please enter your bet");
+    } else if (totalBet == 5000) {
+      alert("All-in!");
+    } else if (totalBet < 5000) {
+      alert("Passing this round");
+    }
+  }
+}
