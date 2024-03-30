1f42d7e1e9bdd555d6bd00be5eea02f9d10d8ebe
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-03-30 17:06:11 +0100

    new folder

---
+++ training-js/js/app.js
@@ -0,0 +1,19 @@
+let character = "Black Widow";
+switch (character) {
+  case "Capitain America":
+    power = "Indestructible shield";
+    break;
+  case "Black Widow":
+    power = "Master spy & assassin";
+    break;
+  case "Iron Man":
+    power = "Genius-level intellect";
+    break;
+  case "Thor":
+    power = "The hammer of Thor";
+    break;
+  default:
+    console.log("Choose your hero!");
+    break;
+}
+console.log(power);
