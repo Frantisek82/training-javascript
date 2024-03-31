3962219724e9b80b57d87b41497ee7c4a243aa93
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-03-31 11:36:02 +0200

    new training exercises

--- training-js/js/app.js
+++ training-js/js/app.js
@@ -1,19 +1,63 @@
-let character = "Black Widow";
-switch (character) {
-  case "Capitain America":
-    power = "Indestructible shield";
-    break;
-  case "Black Widow":
-    power = "Master spy & assassin";
-    break;
-  case "Iron Man":
-    power = "Genius-level intellect";
-    break;
-  case "Thor":
-    power = "The hammer of Thor";
-    break;
-  default:
-    console.log("Choose your hero!");
-    break;
-}
-console.log(power);
+// let character = "Black Widow";
+// switch (character) {
+//   case "Capitain America":
+//     power = "Indestructible shield";
+//     break;
+//   case "Black Widow":
+//     power = "Master spy & assassin";
+//     break;
+//   case "Iron Man":
+//     power = "Genius-level intellect";
+//     break;
+//   case "Thor":
+//     power = "The hammer of Thor";
+//     break;
+//   default:
+//     console.log("Choose your hero!");
+//     break;
+// }
+// console.log(power);
+
+// counting by one to one
+
+// let count = 0;
+// while (count < 10) {
+//   console.log(count);
+//   count++;
+// }
+// console.log("aqui sigue el codigo");
+// console.log(count);
+// console.log("-------------------------")
+
+// counting 2 by 2
+// let number = 0;
+// while (number < 10) {
+//   console.log(number);
+//   number += 2;
+// }
+// console.log("-------------------------")
+
+// counting 5 by 5
+// let number = 0;
+// while (number <= 100) {
+//   console.log(number);
+//   number += 5;
+// }
+// console.log("-------------------------")
+
+// counting multiplicando de dos en dos
+// let number = 2;
+// while (number <= 256) {
+//   console.log(number);
+//   number *= 2;
+// }
+// console.log("-------------------------")
+
+// let counter = 10;
+// let timer = setInterval(() => {
+//   console.log(counter);
+//   counter--;
+//   if (counter < 0) {
+//     clearInterval(timer);
+//   }
+// }, 1000);
