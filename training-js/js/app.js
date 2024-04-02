e059f5ff9a10fb2b6a6eb68dfbe1365c6fe90799
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-02 19:38:26 +0200

    new practising

--- training-js/js/app.js
+++ training-js/js/app.js
@@ -68,21 +68,84 @@
 //   console.log(counter);
 // }
 
-let hoy = "viernes";
+// let hoy = "viernes";
 
-if (
-  hoy == "lunes" ||
-  hoy == "martes" ||
-  hoy == "miércoles" ||
-  hoy == "jueves" ||
-  hoy == "viernes"
-) {
-  console.log("Día de trabajo");
-} else if (hoy == "sábado") {
-  console.log("tareas de casa");
-} else {
-  console.log("Día de descanso");
-}
+// if (
+//   hoy == "lunes" ||
+//   hoy == "martes" ||
+//   hoy == "miércoles" ||
+//   hoy == "jueves" ||
+//   hoy == "viernes"
+// ) {
+//   console.log("Día de trabajo");
+// } else if (hoy == "sábado") {
+//   console.log("tareas de casa");
+// } else {
+//   console.log("Día de descanso");
+// }
 
-if (foo == bar || foo == bar) {
-}
+// if (foo == bar || foo == bar) {
+// }
+
+// let number = 10;
+// while (number > 0) {
+//   number--;
+//   console.log(number);
+// }
+
+//-----------------------------------------------------------
+
+// let number = 2;
+// let multi = 1;
+// while (multi <= 10) {
+//   console.log(number, " x ", multi, " = ", number * multi);
+//   multi++;
+// }
+// console.log("----------------------------------------");
+
+// or
+
+// let numbe = 2;
+// let mult = 1;
+// while (mult <= 10) {
+//   console.log(`${numbe} x ${mult} = ${numbe * mult}`);
+//   mult++;
+// }
+// console.log("----------------------------------------");
+
+//or
+
+// let numb = 2;
+// let mul = 1;
+// while (mul++ < 10) {
+//   console.log(`${numb} x ${mul} = ${numb * mul}`);
+// }
+// console.log('------------------------------------');
+
+//or
+
+// let num = 10;
+// let mu = 1;
+// while (mu++ < 10) {
+//   console.log(`${num} x ${mu} = ${num * mu}`);
+// }
+// console.log('------------------------------------');
+
+//Generating multiplication tables
+// for (let number = 1; number <= 10; number++) {
+//   console.log("Tabla del: ", number);
+//   console.log("-------------------");
+
+//   let multi = 1;
+//   while (multi <= 10) {
+//     console.log(number, "x", multi, "=", number * multi);
+//     multi++;
+//   }
+
+//   console.log("------------------------");
+// }
+
+// let counter = 1;
+// for (;counter <= 10) {
+//   console.log(counter++);
+// }
