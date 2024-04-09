6cf693bf0fd8545fe2cdfc848233531a2ec3c17e
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-09 18:23:53 +0200

    updated exercise 2

--- Ejercicios/Ejercicio 2/js/app_ejercicio2.js
+++ Ejercicios/Ejercicio 2/js/app_ejercicio2.js
@@ -9,37 +9,21 @@ let days = console.log(
 let monthName = prompt("Write any month of the year.");
 switch (monthName) {
   case "January":
-    console.log("January has 31 days");
+  case "March":
+  case "May":
+  case "July":
+  case "October":
+  case "December":
+    alert("This month has 31 days");
     break;
   case "February":
-    console.log("February has 29 days");
-    break;
-  case "March":
-    console.log("March has 31 days");
+    alert("This month has 28 days");
     break;
   case "April":
-    console.log("April has 30 days");
-    break;
-  case "May":
-    console.log("May has 31 days");
-    break;
   case "June":
-    console.log("June has 30 days");
-    break;
-  case "July":
-    console.log("July has 31 days");
-    break;
   case "August":
-    console.log("August has 3o days");
-    break;
-  case "October":
-    console.log("October has 31 days");
-    break;
   case "November":
-    console.log("November has 30 days");
-    break;
-  case "December":
-    console.log("December has 31 days");
+    alert("This month has 30 days");
     break;
   default:
     alert("That's not a real month in english!");
