bf4443c964fb74788a564efc531615f8d8849233
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-09 22:04:24 +0200

    Exercise 5

---
+++ Ejercicios/Ejercicio 5/js/app_ejercicio5.js
@@ -0,0 +1,36 @@
+let tempC1 = -273.15;
+let tempC2 = 0;
+let tempC3 = 37;
+let tempC4 = 100;
+let tempF1 = 0;
+let tempF2 = 0;
+let tempF3 = 0;
+let tempF4 = 0;
+
+tempF1 = tempC1 * (9 / 5) + 32;
+console.log(
+  "Absolute Zero is -273.15 degree Celsius and",
+  tempF1,
+  "degree Fahrenheit."
+);
+
+tempF2 = tempC2 * (9 / 5) + 32;
+console.log(
+  "Freezing point is 0 degree Celsius and",
+  tempF2,
+  "degree Fahrenheit."
+);
+
+tempF3 = tempC3 * (9 / 5) + 32;
+console.log(
+  "Body Temperature is 37 degree Celsius and",
+  tempF3,
+  "degree Fahrenheit."
+);
+
+tempF4 = tempC4 * (9 / 5) + 32;
+console.log(
+  "Boiling point is 100 degree Celsius and",
+  tempF4,
+  "degree Fahrenheit."
+);
