0d8011e834099ae90b9c35926dc6fd3b260397fd
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-09 21:46:20 +0200

    Exercise 4

--- Ejercicios/Ejercicio 3/js/app_ejercicio3.js
+++ Ejercicios/Ejercicio 3/js/app_ejercicio3.js
@@ -3,14 +3,34 @@
 // sea entre 15 grados y 25, lanzar una alerta con el mensaje: Enjoy the weather !”. En caso de que la
 // temperatura sea de más de 25 grados, lanzar la alerta con el mensaje: Cool down.
 
-let outsideTemp = prompt("Please enter the outside temperature.");
+let outsideTemp = prompt("Please, enter the outside temperature.");
 
-if (outsideTemp > 25) {
-  alert("Cool down!");
-} else if (outsideTemp >= 15 || outsideTemp >= 25) {
-  alert("Enjoy the weather!");
-} else if (outsideTemp < 15) {
+if (outsideTemp < 15) {
   alert("Warm up!");
+} else if (outsideTemp >= 15 && outsideTemp <= 25) {
+  alert("Enjoy the weather!");
+} else if (outsideTemp > 25) {
+  alert("Cool down!");
 } else {
-  alert("You did not enter correct outside temperature!");
+  alert("Please, enter a valid temperature!");
 }
+
+//----------------------------------------------------------------------------
+
+// Another and the best version of the exercise-3
+
+// let outsideTemp;
+// do {
+//   outsideTemp = prompt("Please enter the outside temperature.").trim();
+//   if (outsideTemp == "" || isNaN(outsideTemp)) {
+//     alert("Please, write a valid temperature!");
+//   }
+// } while (outsideTemp == "" || isNaN(parseInt(outsideTemp)));
+// // console.log(typeof outsideTemp);
+// if (outsideTemp < 15) {
+//   alert("Warm up!");
+// } else if (outsideTemp >= 15 && outsideTemp <= 25) {
+//   alert("Enjoy the weather!");
+// } else {
+//   alert("Cool down!");
+// }
--- Ejercicios/Ejercicio 4/js/app_ejercicio4.js
+++ Ejercicios/Ejercicio 4/js/app_ejercicio4.js
@@ -1,3 +1,30 @@
 // Escribe un programa que simula un semáforo. El semáforo estará controlado por una variable
-// “centinela” llamada isGreen ”. Si el semáforo está verde, mostrar por consola: “Cross the road ””, en
+// “centinela” llamada isGreen ”. Si el semáforo está verde, mostrar por consola: “Cross the road ”, en
 // caso contrario mostrar: Please await.
+
+// let isGreen = true;
+
+// if (isGreen) {
+//   console.log("Cross the road!");
+// } else {
+//   console.log("Please await!");
+// }
+
+//--------------------------------------------------------------------------
+
+// Another more interesting version of exercise-4
+
+let isGreen = true;
+let counter = 0;
+let timer = setInterval(() => {
+  if (isGreen) {
+    console.log("Cross the road!");
+  } else {
+    console.log("Please await!");
+  }
+
+  isGreen = !isGreen;
+  if (counter++ == 3) {
+    clearInterval(timer);
+  }
+}, 3000);
