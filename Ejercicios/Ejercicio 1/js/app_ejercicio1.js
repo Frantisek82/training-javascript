61c8bac1ad9f28800e820dd0bdd641be8ae5860d
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-09 18:09:57 +0200

    update

--- Ejercicios/Ejercicio 1/js/app_ejercicio1.js
+++ Ejercicios/Ejercicio 1/js/app_ejercicio1.js
@@ -7,60 +7,60 @@
 
 //------------------------------------------------------
 
-// // Another and the best version of the exercise-1
+// Another and the best version of the exercise-1
 
-// const actualDate = new Date();
-// let month = actualDate.getMonth() + 1;
-// console.log("Today is:", actualDate);
-// console.log("The number of the month is:", month);
+const actualDate = new Date();
+let month = actualDate.getMonth() + 1;
+console.log("Today is:", actualDate);
+console.log("The number of the month is:", month);
 
-// switch (month) {
-//   case 1:
-//   case 3:
-//   case 5:
-//   case 7:
-//   case 8:
-//   case 10:
-//   case 12:
-//     days = 31;
-//     break;
-//   case 4:
-//   case 6:
-//   case 9:
-//   case 11:
-//     days = 30;
-//     break;
-//   case 2:
-//     days = 28;
-//     // days = new Date(
-//     //   new Date().getFullYear(),
-//     //   new Date().getMonth(),
-//     //   0
-//     // ).getDate();
-//     break;
-// }
-// console.log("The month has", days, "days.");
+switch (month) {
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
+    days = 31;
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    days = 30;
+    break;
+  case 2:
+    days = 28;
+    // days = new Date(
+    //   new Date().getFullYear(),
+    //   new Date().getMonth(),
+    //   0
+    // ).getDate();
+    break;
+}
+console.log("The month has", days, "days.");
 
 //-------------------------------------------------------
 
-// Another version of exercise-1
+// // Another version of exercise-1
 
-let date = new Date();
-let month = date.getMonth() + 1;
-console.log(month);
+// let date = new Date();
+// let month = date.getMonth() + 1;
+// console.log(month);
 
-if (
-  month == 1 ||
-  month == 3 ||
-  month == 5 ||
-  month == 7 ||
-  month == 8 ||
-  month == 10 ||
-  month == 12
-) {
-  console.log(31);
-} else if (month == 2) {
-  console.log(28);
-} else {
-  console.log(30);
-}
+// if (
+//   month == 1 ||
+//   month == 3 ||
+//   month == 5 ||
+//   month == 7 ||
+//   month == 8 ||
+//   month == 10 ||
+//   month == 12
+// ) {
+//   console.log(31);
+// } else if (month == 2) {
+//   console.log(28);
+// } else {
+//   console.log(30);
+// }
