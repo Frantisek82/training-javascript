6c11d07b2f7df145e6d2b207271940c85b0c8858
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-10 20:19:15 +0200

    updated exercise 9

---
+++ Ejercicios/Ejercicio 10/js/app_ejercicio10.js
@@ -0,0 +1,4 @@
+// Investiga el objeto
+// Math , y genera 2 números aleatorios, entre 1 y 6, para simular un juego de dados.
+// Comprueba los resultados obtenidos y muestra por consola el ganador. Por ejemplo: “Player 1
+// wins !”!”, o draw en caso de empate.
--- Ejercicios/Ejercicio 9/js/app_ejercicio9.js
+++ Ejercicios/Ejercicio 9/js/app_ejercicio9.js
@@ -7,53 +7,84 @@
 
 // El DNI está formado por ocho dígitos y un carácter alfabético de control. Ejemplo: 12345678ª.
 
-let doc = prompt("Please, enter you DNI or NIE").toLowerCase();
-// let doc = "12345678A".toLowerCase();
-// console.log(doc.charCodeAt(7));
-let isDNI = true;
-let validDocument = true;
-//usando ASCII Table
-if (
-  doc.charCodeAt(0) == 120 ||
-  doc.charCodeAt(0) == 121 ||
-  doc.charCodeAt(0) == 122
-) {
-  isDNI = false;
-}
-if (isDNI) {
-  for (let index = 0; index < doc.length; index++) {
-    if (index <= 7) {
-      //usando ASCII Table
-      if (doc.charCodeAt(index) < 48 || doc.charCodeAt(index) > 57) {
-        validDocument = false;
-      }
-    } else {
-      //usando ASCII Table
-      if (doc.charCodeAt(index) < 97 || doc.charCodeAt(index) > 122) {
-        validDocument = false;
-      }
-    }
-  }
-} else {
-  for (let index = 1; index < doc.length; index++) {
-    if (index <= 7) {
-      //usando ASCII Table
-      if (doc.charCodeAt(index) < 48 || doc.charCodeAt(index) > 57) {
-        validDocument = false;
-      }
-    } else {
-      //usando ASCII Table
-      if (doc.charCodeAt(index) < 97 || doc.charCodeAt(index) > 122) {
-        validDocument = false;
-      }
-    }
-  }
-}
+// let doc = prompt("Please, enter you DNI or NIE").toLowerCase();
+// // let doc = "12345678A".toLowerCase();
+// // console.log(doc.charCodeAt(7));
+// let isDNI = true;
+// let validDocument = true;
+// //usando ASCII Table
+// if (
+//   doc.charCodeAt(0) == 120 ||
+//   doc.charCodeAt(0) == 121 ||
+//   doc.charCodeAt(0) == 122
+// ) {
+//   isDNI = false;
+// }
+// if (isDNI) {
+//   for (let index = 0; index < doc.length; index++) {
+//     if (index <= 7) {
+//       //usando ASCII Table
+//       if (doc.charCodeAt(index) < 48 || doc.charCodeAt(index) > 57) {
+//         validDocument = false;
+//       }
+//     } else {
+//       //usando ASCII Table
+//       if (doc.charCodeAt(index) < 97 || doc.charCodeAt(index) > 122) {
+//         validDocument = false;
+//       }
+//     }
+//   }
+// } else {
+//   for (let index = 1; index < doc.length; index++) {
+//     if (index <= 7) {
+//       //usando ASCII Table
+//       if (doc.charCodeAt(index) < 48 || doc.charCodeAt(index) > 57) {
+//         validDocument = false;
+//       }
+//     } else {
+//       //usando ASCII Table
+//       if (doc.charCodeAt(index) < 97 || doc.charCodeAt(index) > 122) {
+//         validDocument = false;
+//       }
+//     }
+//   }
+// }
 
-if (validDocument && isDNI) {
-  console.log("It is a valid DNI!");
-} else if (validDocument) {
-  console.log("It is a valid NIE!");
+// if (validDocument && isDNI) {
+//   console.log("It is a valid DNI!");
+// } else if (validDocument) {
+//   console.log("It is a valid NIE!");
+// } else {
+//   console.log("Not a valid document!");
+// }
+
+//----------------------------------------------------------------------
+
+// Another version of the exercise 9
+
+let userNieDni = prompt("Please, write your DNI or NIE");
+if (userNieDni.length == 9) {
+  let firstCharacter = userNieDni.charCodeAt(0);
+  let lastCharacter = userNieDni.charCodeAt(userNieDni.length - 1);
+  //if NIE
+  if (
+    (firstCharacter == 88 || firstCharacter == 89 || firstCharacter == 90) &&
+    !isNaN(userNieDni.substring(1, userNieDni.length - 1)) &&
+    lastCharacter >= 65 &&
+    lastCharacter <= 90
+  ) {
+    console.log("Your NIE is valid");
+  }
+  //if DNI
+  else if (
+    !isNaN(userNieDni.substring(0, userNieDni.length - 1)) &&
+    lastCharacter >= 65 &&
+    lastCharacter <= 90
+  ) {
+    console.log("Your DNI is valid");
+  } else {
+    console.log("Your DNI or NIE is not valid!");
+  }
 } else {
-  console.log("Not a valid document!");
+  console.log("Your DNI or NIE is not valid!");
 }
