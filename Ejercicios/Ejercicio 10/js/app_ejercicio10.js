95bde59026a94026c9654c16ae6a07036b7afa38
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-15 22:49:20 +0200

    updated exercise 10

--- Ejercicios/Ejercicio 10/js/app_ejercicio10.js
+++ Ejercicios/Ejercicio 10/js/app_ejercicio10.js
@@ -9,17 +9,28 @@ let max = 6;
 let player1 = Math.floor(Math.random() * (max - min + 1)) + min;
 let player2 = Math.floor(Math.random() * (max - min + 1)) + min;
 
-for (let index = 0; index < 10000; index++) {
-  player1 = Math.floor(Math.random() * (max - min + 1)) + min;
-  if (player1 < 1 || player1 > 6) {
-    fails = true;
-  }
+console.log(player1);
+console.log(player2);
+
+if (player1 > player2) {
+  console.log("Player 1 wins!");
+} else if (player1 < player2) {
+  console.log("Player 2 wins!");
+} else {
+  console.log("Draw!");
 }
 
-if (fails) {
-  console.error("test fail");
-} else {
-  console.log("%c test pass", "color: #008000");
-}
+// for (let index = 0; index < 10000; index++) {
+//   player1 = Math.floor(Math.random() * (max - min + 1)) + min;
+//   if (player1 < 1 || player1 > 6) {
+//     fails = true;
+//   }
+// }
+
+// if (fails) {
+//   console.error("test fail");
+// } else {
+//   console.log("%c test pass", "color: #008000");
+// }
 
 console.log(player1);
