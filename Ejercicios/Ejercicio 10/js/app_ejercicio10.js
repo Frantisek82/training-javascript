57701341fe0ff2c4f53c2bb1d75b28809745bf7c
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-15 22:53:05 +0200

    update

--- Ejercicios/Ejercicio 10/js/app_ejercicio10.js
+++ Ejercicios/Ejercicio 10/js/app_ejercicio10.js
@@ -3,22 +3,22 @@
 // Comprueba los resultados obtenidos y muestra por consola el ganador. Por ejemplo: “Player 1
 // wins !”!”, o draw en caso de empate.
 
-let fails = false;
-let min = 1;
-let max = 6;
-let player1 = Math.floor(Math.random() * (max - min + 1)) + min;
-let player2 = Math.floor(Math.random() * (max - min + 1)) + min;
+// let fails = false;
+// let min = 1;
+// let max = 6;
+// let player1 = Math.floor(Math.random() * (max - min + 1)) + min;
+// let player2 = Math.floor(Math.random() * (max - min + 1)) + min;
 
-console.log(player1);
-console.log(player2);
+// console.log(player1);
+// console.log(player2);
 
-if (player1 > player2) {
-  console.log("Player 1 wins!");
-} else if (player1 < player2) {
-  console.log("Player 2 wins!");
-} else {
-  console.log("Draw!");
-}
+// if (player1 > player2) {
+//   console.log("Player 1 wins!");
+// } else if (player1 < player2) {
+//   console.log("Player 2 wins!");
+// } else {
+//   console.log("Draw!");
+// }
 
 // for (let index = 0; index < 10000; index++) {
 //   player1 = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -33,4 +33,4 @@ if (player1 > player2) {
 //   console.log("%c test pass", "color: #008000");
 // }
 
-console.log(player1);
+// console.log(player1);
