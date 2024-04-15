2a4978f942b0505e0ceda9ec3625c0fbaf8023de
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-15 22:42:14 +0200

    exercise 10

--- Ejercicios/Ejercicio 10/js/app_ejercicio10.js
+++ Ejercicios/Ejercicio 10/js/app_ejercicio10.js
@@ -2,3 +2,24 @@
 // Math , y genera 2 números aleatorios, entre 1 y 6, para simular un juego de dados.
 // Comprueba los resultados obtenidos y muestra por consola el ganador. Por ejemplo: “Player 1
 // wins !”!”, o draw en caso de empate.
+
+let fails = false;
+let min = 1;
+let max = 6;
+let player1 = Math.floor(Math.random() * (max - min + 1)) + min;
+let player2 = Math.floor(Math.random() * (max - min + 1)) + min;
+
+for (let index = 0; index < 10000; index++) {
+  player1 = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (player1 < 1 || player1 > 6) {
+    fails = true;
+  }
+}
+
+if (fails) {
+  console.error("test fail");
+} else {
+  console.log("%c test pass", "color: #008000");
+}
+
+console.log(player1);
