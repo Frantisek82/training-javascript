4f884e524a61c0d3a2f54f9d3a923c9c949a1378
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-04-30 00:03:41 +0200

    need to finish exercise 4

---
+++ Ejercicios_Algorithms/Ejercicios Algorithms2/Ejercicio 4/js/algo2_ejercicio4.js
@@ -0,0 +1,25 @@
+// Exercise 4:
+// Escribe un programa con 7 variables con la temperatura de los 7 últimos días (Google). A
+// continuación, muestra cuál ha sido la temperatura media.
+
+function averageTemp() {
+  let mediaTemp = 0;
+  let counter = 0;
+  for (
+    let i = 22,
+      apr22 = 21,
+      apr23 = 20,
+      apr24 = 21,
+      apr25 = 24,
+      apr26 = 24,
+      apr27 = 18,
+      apr28 = 22;
+    i < 32;
+    i++, counter++
+  ) {
+    mediaTemp += eval(`apr${i}`);
+  }
+  console.log("7-Day Average Temperature: ", mediaTemp / counter);
+}
+
+averageTemp();
