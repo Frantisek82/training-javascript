729bb6619ab2ca1925d5c535c3662eeb864a7451
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-05-03 17:53:31 +0200

    start of exercise 8

---
+++ Ejercicios_Algorithms/Ejercicios Algorithms2/Ejercicio 8/js/algo_ejercicio8.js
@@ -0,0 +1,5 @@
+// Exercise 8:
+// Investigando como obtener caracteres a partir de valores ASCII, vamos a crear un generador de contraseñas.
+// En primer lugar, pregunta al usuario cuantos caracteres tendrá la contraseña deseada, (mínimo 8 y máximo 16).
+// A continuación, pregunta si quiere incluir, paso a paso, cada uno de los siguientes caracteres:
+// minúsculas, mayúsculas, números y símbolos.
