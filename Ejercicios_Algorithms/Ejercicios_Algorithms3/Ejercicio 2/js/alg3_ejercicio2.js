11102e7e0c4619ad4f6bdb15fbcf1da7640204d9
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-05-04 11:31:20 +0200

    exercise 2

--- Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 2/js/alg3_ejercicio2.js
+++ Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 2/js/alg3_ejercicio2.js
@@ -6,3 +6,23 @@
 // • Entre las 18:00 y las 04:59 => "Good Evening, {userName}!"
 
 // Los saludos deberán estar predefinidos en un objeto con el identificador "greeting".
+
+function personalizadoGreeting() {
+  let date = new Date();
+  console.log("Today is: ", date);
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let userName = prompt("What is your name?");
+
+  if (hour >= 6 && minutes >= 0 && hour <= 11 && minutes <= 59) {
+    alert(`Good Morning, ${userName}!`);
+  } else if (hour >= 12 && minutes >= 0 && hour <= 20 && minutes <= 59) {
+    alert(`Good Afternoon, ${userName}!`);
+  } else if (hour >= 21 && minutes >= 0 && hour <= 5 && minutes <= 59) {
+    alert(`Good Evening, ${userName}!`);
+  } else {
+    console.log("SYSTEM TIME IS FAILING");
+  }
+}
+
+personalizadoGreeting();
