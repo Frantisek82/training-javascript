0a366f8bc7c73f0644dc3803c5314d8f32c5b097
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-05-04 23:38:51 +0200

    start of exercise 5

---
+++ Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 5/js/alg3_ejercicio5.js
@@ -0,0 +1,16 @@
+// Exercise 5:
+
+// Escribe un programa que genera matrículas aleatorias, sabiendo que:
+// • Una matrícula válida debe estar compuesta por 4 números y 3 letras.
+// • Los números pueden ir desde 0000 hasta 9999
+// • Las letras pueden ir desde BBB hasta ZZZ
+// • No se permiten vocales
+// • No se permiten las consonantes Ñ ni Q
+// El número de matrículas aleatorias será a petición del usuario.
+
+function randomLicensePlates() {
+  let plateNumber = prompt(
+    "Please, enter 4 numbers to generate a license plate: "
+  );
+  let plate = "";
+}
