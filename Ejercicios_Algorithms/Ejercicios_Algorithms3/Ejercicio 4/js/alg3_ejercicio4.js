93e35f156ca8434456105473be73dec533580425
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-05-04 22:49:57 +0200

    start of exercise 4

---
+++ Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 4/js/alg3_ejercicio4.js
@@ -0,0 +1,12 @@
+// Exercise 4:
+
+// Escribe un programa que almacena un DNI dado por el usuario, y verifica si es un DNI válido, lanzando una alerta "Valid DNI" o "The data entered is wrong".
+
+// Para verificar el DNI, se divide el número entre 23 y el resto se sustituye por una letra que se determina por la siguiente tabla:
+
+// | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 |  
+// | T | R | W | A | G | M | Y | F | P | D | X  | B  |
+
+// | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 |
+// | N  | J  | Z  | S  | Q  | V  | H  | L  | C  | K  | E  |
+
