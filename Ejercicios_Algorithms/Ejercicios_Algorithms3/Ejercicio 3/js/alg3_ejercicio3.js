05730d1fca8ec030fe3478e326814267dc799275
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-05-04 22:33:38 +0200

    Exercise 3

--- Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 3/js/alg3_ejercicio3.js
+++ Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 3/js/alg3_ejercicio3.js
@@ -3,5 +3,22 @@
 // Escribe un programa que genere 10 códigos hexadecimales aleatorios (no repetidos), y escriba en la consola “ Hello World !” del color aleatorio generado.
 
 function colorGenerator() {
-    
+  function hexCodeGenerator() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  for (let i = 0; i <= 10; i++) {
+    console.log(
+      "%cHello World!",
+      `color: ${hexCodeGenerator()}`,
+      `in color: ${hexCodeGenerator()}`
+    );
+  }
 }
+
+colorGenerator();
