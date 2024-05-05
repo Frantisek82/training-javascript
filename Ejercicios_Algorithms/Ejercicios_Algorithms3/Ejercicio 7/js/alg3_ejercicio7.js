d7aa67a0530f550fc730f887093a943f460fbedb
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-05-05 23:29:03 +0200

    Exercise 7

--- Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 7/js/alg3_ejercicio7.js
+++ Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 7/js/alg3_ejercicio7.js
@@ -2,3 +2,16 @@
 
 // Escribe un programa que genera 100 números aleatorios, entre 0 y 500, y los almacena en un array.
 // A continuación filtra todos los números impares, ordenando los pares de mayor a menor.
+
+function evenNumbersGenerator() {
+  let numbers = new Array(100);
+
+  for (let index = 0; index < numbers.length; index++) {
+    numbers[index] = Math.floor(Math.random() * 501);
+  }
+  numbers = numbers.filter((number) => number % 2 === 0);
+  numbers = numbers.sort((a, b) => b - a);
+  console.log(numbers);
+}
+
+evenNumbersGenerator();
