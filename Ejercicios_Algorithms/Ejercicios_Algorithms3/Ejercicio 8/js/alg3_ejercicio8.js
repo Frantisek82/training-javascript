87cc86628f04fc1bab8f696fc3d128622b32f357
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-05-08 22:27:40 +0200

    Exercise 8

--- Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 8/js/alg3_ejercicio8.js
+++ Ejercicios_Algorithms/Ejercicios_Algorithms3/Ejercicio 8/js/alg3_ejercicio8.js
@@ -16,5 +16,73 @@
 // En caso de perder todo el saldo, la partida finaliza. En caso de retirarse, se calcula si ha habido beneficios o pérdidas, y se indica mediante una alerta: "Betting benefits : XXX €", mostrando las pérdidas con un número negativo, y las ganacias con un número positivo. A continuación, otra alerta con el saldo total: "Total balance: YYY €"
 
 function playTheHighestCard() {
-    
+  let suits = ["clubs", "hearts", "spades", "diamonds"];
+  let clubs = [
+    "ACE",
+    "KING",
+    "QUEEN",
+    "JACK",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+  ];
+  let hearts = [...clubs];
+  let spades = Array.of(...clubs);
+  let diamonds = Array.from(clubs);
+
+  // diamonds[0] = "test";
+  console.log(diamonds);
+  console.log(clubs);
+  let balance = 500;
+  let bet = 0;
+  let minimumBet = 5;
+  let continuePlaying = false;
+
+  do {
+    do {
+      bet = parseInt(prompt("How much do you want to bet?"));
+      if (bet > balance) {
+        alert("Sorry, your bet is higher than your available balance.");
+        alert(`Maximum available: ${balance}`);
+      }
+      if (bet < minimumBet) {
+        alert("Sorry, your bet is higher than your available balance.");
+        alert(`Minimum required: ${minimumBet}`);
+      }
+    } while (bet > balance || bet < minimumBet);
+    let card1 = Math.floor(Math.random() * clubs.length);
+    let card2 = Math.floor(Math.random() * clubs.length);
+    let suit1 = suits[Math.floor(Math.random() * suits.length)];
+    let suit2 = "";
+    do {
+      suit2 = suits[Math.floor(Math.random() * suits.length)];
+    } while (card1 == card2 && suit1 == suit2);
+    console.log(
+      `${eval(suit1)[card1]} ${suit1} vs. ${eval(suit2)[card2]} ${suit2}`
+    );
+
+    if (card1 < card2) {
+      balance += bet;
+      alert("You win!");
+    } else if (card1 > card2) {
+      balance -= bet;
+      alert("You lose!");
+    } else {
+      alert("Draw!");
+    }
+    if (balance >= minimumBet) {
+      continuePlaying =
+        prompt("Would like to continue playing").toLowerCase() == "y";
+    }
+  } while (continuePlaying && balance >= minimumBet);
+  alert(`Betting benefits: ${balance - 500} €`);
+  alert(`Total balance: ${balance} €`);
 }
+
+playTheHighestCard();
