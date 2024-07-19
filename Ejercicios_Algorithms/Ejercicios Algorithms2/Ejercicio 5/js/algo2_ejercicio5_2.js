b74344ac0ae0730e2c3732c3216a5aeec3eb93fb
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-07-20 01:18:32 +0200

    updated ejercicio5

--- Ejercicios_Algorithms/Ejercicios Algorithms2/Ejercicio 5/js/algo2_ejercicio5_2.js
+++ Ejercicios_Algorithms/Ejercicios Algorithms2/Ejercicio 5/js/algo2_ejercicio5_2.js
@@ -27,3 +27,125 @@
 // ** El programa deberá registrar el tiempo de realización, es decir, desde que comienza el proceso de transformación del string hasta que termina. Después de los string requeridos, el programa mostrará las siguientes líneas: **
 // • 5 words , 25 chars
 // • work done in 750 miliseconds
+
+const exercise5 = () => {
+  let words;
+  let chars;
+  let input;
+  let questions = {
+    replace: null,
+    capitalize: null,
+    commas: null,
+  };
+  let sentence = prompt("Enter a sentence");
+
+  do {
+    input = prompt("Replace chars by signs?\nY/N");
+    if (input == "Y") {
+      questions.replace = true;
+    } else {
+      questions.replace = false;
+    }
+  } while (input != "Y" && input != "N");
+  do {
+    input = prompt("Capitalize?\nY/N");
+    if (input == "Y") {
+      questions.capitalize = true;
+    } else {
+      questions.capitalize = false;
+    }
+  } while (input != "Y" && input != "N");
+  do {
+    input = prompt("Replace spaces with commas?\nY/N");
+    if (input == "Y") {
+      questions.commas = true;
+    } else {
+      questions.commas = false;
+    }
+  } while (input != "Y" && input != "N");
+
+  let timerStart = performance.now();
+  stringManager(sentence, questions);
+  let timerEnd = performance.now() - timerStart;
+  console.log(
+    "%cWork done in:",
+    "color: black; background-color: Yellow; font-wight: bold",
+    timerEnd,
+    "ms"
+  );
+
+  // Main function
+  function stringManager(sentence, questions) {
+    console.log(
+      "%cEntered sentence:",
+      "color: black; background-color: Yellow; font-wight: bold"
+    );
+    console.log(sentence);
+    if (questions.replace == true) {
+      console.log(
+        "%cChars replaced by signs:",
+        "color: black; background-color: Yellow; font-wight: bold"
+      );
+      replace(sentence);
+    }
+    if (questions.capitalize == true) {
+      console.log(
+        "%cFirst letter of the word capitalized:",
+        "color: black; background-color: Yellow; font-wight: bold"
+      );
+      capitalize(sentence);
+    }
+    if (questions.commas == true) {
+      console.log(
+        "%cSpaces replaced by commas:",
+        "color: black; background-color: Yellow; font-wight: bold"
+      );
+      commas(sentence);
+    }
+    count(sentence);
+    console.log(
+      "%cWords:",
+      "color: black; background-color: Yellow; font-wight: bold",
+      words
+    );
+    console.log(
+      "%cChars:",
+      "color: black; background-color: Yellow; font-wight: bold",
+      chars
+    );
+  }
+
+  // Replace function
+  function replace(sentence) {
+    let replaceRule = { a: `@`, e: `3`, i: `1`, o: `0`, s: `$` };
+    let replaceResult = sentence.replace(/a|e|i|o|s/g, function (matched) {
+      return replaceRule[matched];
+    });
+    console.log(replaceResult);
+  }
+
+  // Capitalize function
+  function capitalize(sentence) {
+    let capitalizeResult = sentence.split(" ");
+    for (let i = 0; i < capitalizeResult.length; i++) {
+      capitalizeResult[i] =
+        capitalizeResult[i][0].toUpperCase() + capitalizeResult[i].substr(1);
+    }
+    capitalizeResult = capitalizeResult.join(" ");
+    console.log(capitalizeResult);
+  }
+
+  // Commas function
+  function commas(sentence) {
+    let commasResult = sentence.replace(/,/g, "");
+    commasResult = commasResult.replace(/ /g, ",");
+    console.log(commasResult);
+  }
+
+  // Count function
+  function count(sentence) {
+    words = sentence.split(" ").length;
+    chars = sentence.split("").length;
+  }
+};
+exercise5();
