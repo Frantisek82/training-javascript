8a8151df5300304c73743987774ec11112f62054
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-07-20 02:35:17 +0200

    updated alg2 ejercicio5

--- Ejercicios_Algorithms/Ejercicios Algorithms2/Ejercicio 5/js/algo2_ejercicio5.js
+++ Ejercicios_Algorithms/Ejercicios Algorithms2/Ejercicio 5/js/algo2_ejercicio5.js
@@ -28,9 +28,11 @@
 // • 5 words , 25 chars
 // • work done in 750 miliseconds
 
-function stringManager() {
+function exercise5() {
   let startTime = 0,
     endTime = 0,
+    startTime2 = 0,
+    endTime2 = 0,
     words = 0,
     chars = 0;
   let response = "";
@@ -47,53 +49,36 @@ function stringManager() {
   let string = "";
   do {
     string = prompt("Please, enter you string.").trim();
-    console.log(string);
+    // console.log(string);
   } while (!string || string.length == 0);
   words = string.split(" ").length;
   chars = [...string].length;
   for (let index = 0; index < questions.length; index++) {
     do {
       response = prompt(questions[index]).trim();
+      //   console.log(response);
       switch (index) {
         case 0:
           options.replaceChars = response == "y" ?? false;
-          startTime = performance.now();
-          console.log(
-            string
-              .replaceAll("a", "@")
-              .replaceAll("e", "3")
-              .replaceAll("i", "1")
-              .replaceAll("o", "0")
-              .replaceAll("s", "$")
-          );
-          endTime = performance.now();
-          console.log(`${words} words, ${chars} chars`);
-          console.log(`Work done in ${endTime - startTime} miliseconds`);
           break;
         case 1:
           options.capitalize = response == "y" ?? false;
-          startTime = performance.now();
-          console.log(
-            string.replace(/(^\w|\s\w)/g, (letter) => letter.toUpperCase())
-          );
-          endTime = performance.now();
-          console.log(`${words} words, ${chars} chars`);
-          console.log(`Work done in ${endTime - startTime} miliseconds`);
           break;
         case 2:
           options.replaceSpaces = response == "y" ?? false;
-          startTime = performance.now();
-          console.log(string.replace(/\s/g, ","));
-          endTime = performance.now();
-          console.log(`${words} words, ${chars} chars`);
-          console.log(`Work done in ${endTime - startTime} miliseconds`);
           break;
       }
     } while (!response || response.length == 0 || !/y|n/i.test(response));
   }
-  //   startTime = performance.now();
-  //   endTime = performance.now();
-  //   console.log(`${words} words, ${chars} chars`);
+  startTime = performance.now();
+  console.time("work done in ");
+  startTime2 = new Date().getTime();
+  stringManager(string, options);
+  endTime = performance.now();
+  console.log(`${words} words, ${chars} chars`);
+  console.timeEnd("work done in ");
+  endTime2 = new Date().getTime();
+  console.log(`with performance: ${endTime - startTime} ms`);
+  console.log(`with Date(): ${endTime2 - startTime2} ms`);
 }
-
-stringManager();
+exercise5();
