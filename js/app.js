e9f4867ecb4b0c9e6372aeb8fece8a8503115735
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-07-29 12:13:36 +0200

    fininished practicing Destructuring arrays

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("working");
+// let [num1, num2] = [1, 2];
+// [num1, num2] = [11];
+// [num1, num2] = [7, 9, 8];
+// [, num1, , , , num2] = [4, 6, 7, 9, 15, -8, 20];
+// let num3;
+// [num1, , , num2, , ...num3] = [1, 2, 3, 4, 5, 6, 7, 8];
+// [num1, [num2, num3]] = [3, [2.5, 9], 6, 7, 8];
+// let num4;
+// // [num1, [, num2], num3, ...num4] = [3, [2.5, 9], 6, 7, 8];
+// [num1, [, num2], num3] = [3, [2.5, 9], 6, 7, 8];
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+// let [start, , , middle, ...rest] = "Hello";
+// console.log(start);
+// console.log(middle);
+// console.log(rest);
+
+// Desestructuracion objetos
