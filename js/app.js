2b892cdb12200c764769cde0cafa944b9c99b60a
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-07-30 10:57:03 +0200

    localStorage

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
-// // Destructuring arrays
+// Destructuring arrays
 
-// // let [num1, num2] = [1, 2];
-// // [num1, num2] = [11];
-// // [num1, num2] = [7, 9, 8];
-// // [, num1, , , , num2] = [4, 6, 7, 9, 15, -8, 20];
-// // let num3;
-// // [num1, , , num2, , ...num3] = [1, 2, 3, 4, 5, 6, 7, 8];
-// // [num1, [num2, num3]] = [3, [2.5, 9], 6, 7, 8];
-// // let num4;
-// // // [num1, [, num2], num3, ...num4] = [3, [2.5, 9], 6, 7, 8];
-// // [num1, [, num2], num3] = [3, [2.5, 9], 6, 7, 8];
+// let [num1, num2] = [1, 2];
+// [num1, num2] = [11];
+// [num1, num2] = [7, 9, 8];
+// [, num1, , , , num2] = [4, 6, 7, 9, 15, -8, 20];
+// let num3;
+// [num1, , , num2, , ...num3] = [1, 2, 3, 4, 5, 6, 7, 8];
+// [num1, [num2, num3]] = [3, [2.5, 9], 6, 7, 8];
+// let num4;
+// // [num1, [, num2], num3, ...num4] = [3, [2.5, 9], 6, 7, 8];
+// [num1, [, num2], num3] = [3, [2.5, 9], 6, 7, 8];
 
-// // console.log(num1);
-// // console.log(num2);
-// // console.log(num3);
-// // console.log(num4);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
 
-// // let [start, , , middle, ...rest] = "Hello";
-// // console.log(start);
-// // console.log(middle);
-// // console.log(rest);
+// let [start, , , middle, ...rest] = "Hello";
+// console.log(start);
+// console.log(middle);
+// console.log(rest);
 
-// // Desestructuracion objetos
+// Desestructuracion objetos
 
 // let hero = {
 //   character: "Doctor Strange",
@@ -33,22 +33,22 @@
 // };
 
 // let { name } = hero;
-// // let { artifacts, ...otherPowers } = hero;
-// // console.log(artifacts);
-// // console.log(otherPowers);
+// let { artifacts, ...otherPowers } = hero;
+// console.log(artifacts);
+// console.log(otherPowers);
 
-// // Destructurar obteniendo el caracter, los poderes en dos variables independientes y el resto del objeto
+// Destructurar obteniendo el caracter, los poderes en dos variables independientes y el resto del objeto
 
-// // let {
-// //   character,
-// //   powers: { artifacts, skills },
-// //   ...restObject
-// // } = hero;
+// let {
+//   character,
+//   powers: { artifacts, skills },
+//   ...restObject
+// } = hero;
 
-// // console.log(character);
-// // console.log(artifacts);
-// // console.log(skills);
-// // console.log(restObject);
+// console.log(character);
+// console.log(artifacts);
+// console.log(skills);
+// console.log(restObject);
 
 // let {
 //   powers: {
@@ -69,3 +69,14 @@
 // console.log(jsonText);
 // let anotherPerson = JSON.parse(jsonText);
 // console.log(anotherPerson.lastName);
+
+let userName = prompt("What is your name?");
+let phone = prompt("What is your phone number?");
+
+localStorage.name = userName;
+localStorage.phone = phone;
+
+console.log(localStorage.name);
+console.log(localStorage.phone);
+
+localStorage.clear();
